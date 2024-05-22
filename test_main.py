a2b12841ada709be44e618938c9946e003fee286
Klonopin <92847399+korkysha@users.noreply.github.com>
2024-05-22 17:18:17 +0000

    sd

--- test_main.py
+++ test_main.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 
 
 class TestSimpleApp(TestCase):
-    def test_print_message(self):
-        self.assertEqual("Hello World", "Hello World")
-        
+    def test_sum(self):
+        self.assertEqual(2 + 3, 5)
+
+    def test_list_contains_element(self):
+        self.assertIn(3, [1, 2, 3, 4, 5])
